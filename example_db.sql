91ad01e91b7b43d909218c07e75544f5ca42cfdd
jonecole <jonecole@oregonstate.edu>
2020-03-09 18:16:41 -0700

    Added SQL file containing sample tables and data.

---
+++ example_db.sql
@@ -0,0 +1,104 @@
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `ex_author`
+--
+DROP TABLE IF EXISTS `ex_author`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `ex_author` (
+  `authorID` int(10) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `dob` date NOT NULL,
+  `dod` date DEFAULT NULL,
+  PRIMARY KEY (`authorID`)
+) AUTO_INCREMENT=1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Sample data for table `ex_author`
+--
+LOCK TABLES `ex_author` WRITE;
+/*!40000 ALTER TABLE `ex_author` DISABLE KEYS */;
+INSERT INTO `ex_author` VALUES
+  (1,'Orwell, George','1903-06-25','1950-01-21'),
+  (2,'Hawking, Stephen','1942-01-08','2018-03-14'),
+  (3,'Dawkins, Richard','1941-03-26',NULL),
+  (4,'Fischer, David Hackett','1935-12-02',NULL),
+  (5,'Herbert, Franklin Patrick Jr.','1920-10-08','1986-02-11');
+/*!40000 ALTER TABLE `ex_author` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
+
+--
+-- Table structure for table `ex_author`
+--
+DROP TABLE IF EXISTS `ex_publisher`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `ex_publisher` (
+  `publisherID` int(10) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  PRIMARY KEY (`publisherID`)
+) AUTO_INCREMENT=1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Sample data for table `ex_publisher`
+--
+LOCK TABLES `ex_publisher` WRITE;
+/*!40000 ALTER TABLE `ex_publisher` DISABLE KEYS */;
+INSERT INTO `ex_publisher` VALUES
+  (1,'Harvill Secker'),
+  (2,'Bantam Books'),
+  (3,'Oxford University Press'),
+  (4,'Chilton Company');
+/*!40000 ALTER TABLE `ex_publisher` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
+
+--
+-- Table structure for table `ex_book`
+--
+DROP TABLE IF EXISTS `ex_book`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `ex_book` (
+  `ISBN` bigint NOT NULL,
+  `date_published` date DEFAULT NULL,
+  `title` varchar(255) NOT NULL,
+  `genre` varchar(255) NOT NULL,
+  `authorID` int(10) NOT NULL,
+  `publisherID` int(10) NOT NULL,
+  PRIMARY KEY (`ISBN`),
+  FOREIGN KEY (`authorID`) REFERENCES ex_author(`authorID`),
+  FOREIGN KEY (`publisherID`) REFERENCES ex_publisher(`publisherID`)
+);
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Sample data for table `ex_book`
+--
+LOCK TABLES `ex_book` WRITE;
+/*!40000 ALTER TABLE `ex_book` DISABLE KEYS */;
+INSERT INTO `ex_book` VALUES
+  (9780241416419,'1949-06-08','Nineteen Eighty-Four','Political Fiction',1,1),
+  (9781846553547,'1945-08-17','Animal Farm','Political Satire',1,1),
+  (9780553380163,'1988-03-01','A Brief History of Time','Cosmology',2,2),
+  (9780618680009,'2006-10-02','The God Delusion','Non-fiction',3,2),
+  (9780192860927,'1990-10-25','The Selfish Gene','Popular Science',3,3),
+  (9780195069051,'1989-03-14','Albion\'s Seed: Four British Folways in America','Non-fiction',4,3),
+  (9780195098310,'1995-04-19','Paul Revere\'s Ride','Biography',4,3),
+  (9780441172719,'1965-08-01','Dune','Science Fiction',5,4);
+/*!40000 ALTER TABLE `ex_book` ENABLE KEYS */;
+UNLOCK TABLES;

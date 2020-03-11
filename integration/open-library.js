af764b38d9146b069c6e78a8f706e7940a991f64
Khoa Tran <k.tran.0918@outlook.com>
2020-03-10 22:17:50 -0700

    implement nested loop for two-letter queries

--- integration/open-library.js
+++ integration/open-library.js
@@ -8,34 +8,36 @@ const bookEntries = [];
 async function getBooks() {
   // In ASCII, 'A' is 65 and 'Z' is 90
   for (let i = 65; i <= 90; i++) {
-    try {
-      let response = await request({
-        uri: 'http://openlibrary.org/search.json',
-        qs: {
-          // Add extra alphabet letters to the query for more data
-          title: String.fromCharCode(i) + 'a',  // search query
+    for (let j = 65; j <= 90; j++) {
+      try {
+        let response = await request({
+          uri: 'http://openlibrary.org/search.json',
+          qs: {
+            // Add extra alphabet letters to the query for more data
+            title: String.fromCharCode(i) + String.fromCharCode(j),  // search query
+          }
+        });
+        // console.log(response);
+        response = JSON.parse(response);
+        let books = response.docs;
+        // console.log(books);
+        if (books) {
+          for (let book of books) {
+            let entry = {
+              title: book.title_suggest,
+              isbn: book.isbn && book.isbn[0],
+              author_name: book.author_name && book.author_name[0],
+              publish_date: book.publish_date && book.publish_date[0],
+              publisher: book.publisher && book.publisher[0]
+            };
+            // console.log(entry);
+            bookEntries.push(entry);
+          }
         }
-      });
-      // console.log(response);
-      response = JSON.parse(response);
-      let books = response.docs;
-      // console.log(books);
-      if (books) {
-        for (let book of books) {
-          let entry = {
-            title: book.title_suggest,
-            isbn: book.isbn && book.isbn[0],
-            author_name: book.author_name && book.author_name[0],
-            publish_date: book.publish_date && book.publish_date[0],
-            publisher: book.publisher && book.publisher[0]
-          };
-          // console.log(entry);
-          bookEntries.push(entry);
-        }
-      }
 
-    } catch (err) {
-      console.error(err);
+      } catch (err) {
+        console.error(err);
+      }
     }
   }
 

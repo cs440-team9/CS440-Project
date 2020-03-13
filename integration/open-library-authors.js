3aa76d8b9212afdab8da4b34557727e96f17ea54
Khoa Tran <k.tran.0918@outlook.com>
2020-03-12 18:20:22 -0700

    implement script for pulling books, authors, and publishers separately from Open Library

--- integration/open-library-authors.js
+++ integration/open-library-authors.js
@@ -1,8 +1,6 @@
 const request = require('request-promise-native');
-const sql = require('mysql');
-const ObjectsToCsv = require('objects-to-csv');
-
 const mysql = require('mysql');
+
 const sqlPool = mysql.createPool({
   connectionLimit: 10,
   host: 'classmysql.engr.oregonstate.edu',
@@ -14,7 +12,6 @@ const sqlPool = mysql.createPool({
 // import credentials
 // const { openlibrary } = require('./config').credentials;
 
-// const bookEntries = [];
 
 async function getBooks() {
   let bookEntries = [];
@@ -36,11 +33,7 @@ async function getBooks() {
       if (books) {
         for (let book of books) {
           let entry = {
-            title: book.title_suggest,
-            isbn: book.isbn && book.isbn[0],
-            author: book.author_name && book.author_name[0],
-            publisher: book.publisher && book.publisher[0],
-            date_published: book.publish_year && book.publish_year[0]
+            author: book.author_name && book.author_name[0]
           };
           // console.log(entry);
           bookEntries.push(entry);
@@ -61,6 +54,9 @@ async function main() {
     console.log('Getting books...');
     let bookEntries = await getBooks();
 
+    authorEntries = bookEntries.map(entry => entry.author);
+    authorUniqueEntries = [...new Set(authorEntries)];
+
     // console.log('Writing entries to CSV');
     // const csv = new ObjectsToCsv(bookEntries);
     // await csv.toDisk('./open-library.csv');
@@ -68,36 +64,37 @@ async function main() {
     sqlPool.getConnection((err, connection) => {
       // catch err needed?
 
-      const query = `INSERT INTO ex_book_test(ISBN, title, year_published, authorID, publisherID) VALUES (?, ?, ?, ?, ?)`;
+      const query = `INSERT INTO ex_author_test(name) VALUES (?)`;
 
-      for (let entry of bookEntries) {
-        // console.log(entry.isbn);
-        const getAuthorQuery = `SELECT authorID from ex_author_test WHERE name="${entry.author}"`;
-        const getPublisherQuery = `SELECT publisherID from ex_publisher_test WHERE name="${entry.publisher}"`;
-
+      for (let entry of authorUniqueEntries) {
+        // Check for author names already in the DB
+        const getAuthorQuery = `SELECT name from ex_author_test WHERE name="${entry}"`;
         connection.query(getAuthorQuery, (err, result) => {
-          // console.log(result);
-          let authorID = result && result.length && result[0].authorID || null;
-          // console.log(authorID);
+          // if an author name is returned from the DB, this means it's a duplicate
+          // Skip it
+          let authorName = result && result.length && result[0].name || null;
+          // console.log(authorName);
+          if (entry === authorName) {
+            return;
+          }
 
-          connection.query(getPublisherQuery, (err, result) => {
-            let publisherID = result && result.length && result[0].publisherID || null;
-            const { isbn, title, date_published } = entry;
-
-            connection.query(query, [isbn, title, date_published, authorID, publisherID], function (err, result) {
-              if (err) {
-                console.error(err);
-              } else {
-                console.log('Successfully inserted', title);
-              }
-            });
+          // Add new author
+          connection.query(query, [entry], function (err, result) {
+            if (err) {
+              console.error(err);
+            } else {
+              console.log('Successfully inserted', entry);
+            }
           });
+
+
         });
       }
 
       connection.release();
     });
 
+    // sqlPool.end();
   } catch (err) {
     console.error(err);
   }
--- integration/open-library-publishers.js
+++ integration/open-library-publishers.js
@@ -1,8 +1,6 @@
 const request = require('request-promise-native');
-const sql = require('mysql');
-const ObjectsToCsv = require('objects-to-csv');
-
 const mysql = require('mysql');
+
 const sqlPool = mysql.createPool({
   connectionLimit: 10,
   host: 'classmysql.engr.oregonstate.edu',
@@ -14,7 +12,6 @@ const sqlPool = mysql.createPool({
 // import credentials
 // const { openlibrary } = require('./config').credentials;
 
-// const bookEntries = [];
 
 async function getBooks() {
   let bookEntries = [];
@@ -35,12 +32,9 @@ async function getBooks() {
       // console.log(books);
       if (books) {
         for (let book of books) {
+          // console.log(book);
           let entry = {
-            title: book.title_suggest,
-            isbn: book.isbn && book.isbn[0],
-            author: book.author_name && book.author_name[0],
-            publisher: book.publisher && book.publisher[0],
-            date_published: book.publish_year && book.publish_year[0]
+            publisher: book.publisher && book.publisher.length && book.publisher[0]
           };
           // console.log(entry);
           bookEntries.push(entry);
@@ -61,6 +55,9 @@ async function main() {
     console.log('Getting books...');
     let bookEntries = await getBooks();
 
+    publisherEntries = bookEntries.map(entry => entry.publisher);
+    publisherUniqueEntries = [...new Set(publisherEntries)];
+
     // console.log('Writing entries to CSV');
     // const csv = new ObjectsToCsv(bookEntries);
     // await csv.toDisk('./open-library.csv');
@@ -68,36 +65,37 @@ async function main() {
     sqlPool.getConnection((err, connection) => {
       // catch err needed?
 
-      const query = `INSERT INTO ex_book_test(ISBN, title, year_published, authorID, publisherID) VALUES (?, ?, ?, ?, ?)`;
+      const query = `INSERT INTO ex_publisher_test(name) VALUES (?)`;
 
-      for (let entry of bookEntries) {
-        // console.log(entry.isbn);
-        const getAuthorQuery = `SELECT authorID from ex_author_test WHERE name="${entry.author}"`;
-        const getPublisherQuery = `SELECT publisherID from ex_publisher_test WHERE name="${entry.publisher}"`;
+      for (let entry of publisherUniqueEntries) {
+        // Check for publisher names already in the DB
+        const getPublisherQuery = `SELECT name from ex_publisher_test WHERE name="${entry}"`;
+        connection.query(getPublisherQuery, (err, result) => {
+          // if an publisher name is returned from the DB, this means it's a duplicate
+          // Skip it
+          let publisherName = result && result.length && result[0].name || null;
+          // console.log(publisherName);
+          if (entry === publisherName) {
+            return;
+          }
 
-        connection.query(getAuthorQuery, (err, result) => {
-          // console.log(result);
-          let authorID = result && result.length && result[0].authorID || null;
-          // console.log(authorID);
-
-          connection.query(getPublisherQuery, (err, result) => {
-            let publisherID = result && result.length && result[0].publisherID || null;
-            const { isbn, title, date_published } = entry;
-
-            connection.query(query, [isbn, title, date_published, authorID, publisherID], function (err, result) {
-              if (err) {
-                console.error(err);
-              } else {
-                console.log('Successfully inserted', title);
-              }
-            });
+          // Add new publisher
+          connection.query(query, [entry], function (err, result) {
+            if (err) {
+              console.error(err);
+            } else {
+              console.log('Successfully inserted', entry);
+            }
           });
+
+
         });
       }
 
       connection.release();
     });
 
+    // sqlPool.end();
   } catch (err) {
     console.error(err);
   }
